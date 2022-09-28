778e0bc7828c868959eca021d7ff09fde0f0adad
Vansh-Gupta4 <gvansh.411@gmail.com>
2022-09-28 16:10:24 +0000

    final

---
+++ frontend/build/precache-manifest.8c80a263280ccfbecdccf9693e8f5e7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49621d1e3990f4f9f433cc7d96cdd256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3c3dfa6dd248705f1a0",
+    "url": "/static/css/main.184483fa.chunk.css"
+  },
+  {
+    "revision": "c081cd8419c5c48478ba",
+    "url": "/static/js/2.16bf68e5.chunk.js"
+  },
+  {
+    "revision": "f7d13dfba7c11f9ea8f09ce8fb2163d2",
+    "url": "/static/js/2.16bf68e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3c3dfa6dd248705f1a0",
+    "url": "/static/js/main.5d323dc5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.5d323dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef26fcae5de950dd74c",
+    "url": "/static/js/runtime-main.b1b66aad.js"
+  }
+]);
